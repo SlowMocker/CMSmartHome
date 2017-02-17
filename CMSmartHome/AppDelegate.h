14e492d0f8033b8f950922d3483c0f09e7085ee2
Smicro <M_wuwenhao@163.com>
2017-02-17 14:53:55 +0800

    创建工程

---
+++ CMSmartHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMSmartHome
+//
+//  Created by Wu on 17/2/17.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CMSmartHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CMSmartHome
+//
+//  Created by Wu on 17/2/17.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
